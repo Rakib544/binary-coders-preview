85bf0dd6adc6bdcef19e4e669a72a8f07fca7c02
tanvir1017 <tanvir.hossaindev1@gmail.com>
2023-06-16 21:51:42 +0600

    tanvir added our vision section

--- app/components/Vision/vision.tsx
+++ app/components/Vision/vision.tsx
@@ -1,10 +1,7 @@
-import { blurImgUrl } from 'data/blur-img-url'
-import { stackCard } from 'data/navbar'
 import { motion } from 'framer-motion'
-import BlurrableImage from '../blurable-img'
 const Vision = () => {
   return (
-    <div className='container'>
+    <div className='container '>
       <div className='px-4 md:px-12 mt-32'>
         <motion.div
           className='mx-auto w-11/12 mt-24 md:mb-16 md:mt-32 '
@@ -13,43 +10,117 @@ const Vision = () => {
           viewport={{ once: true }}
           transition={{ duration: 0.5 }}
         >
-          <div>
-            <h2 className='font-SpaceGroteskBold text-5xl leading-[4.2rem]'>
-              {`>`}_ Academy.py at a glance <br />
-              Let's have a look. How exciting its <br /> journey going to be
+          <div className='Title'>
+            <h2 className='font-SpaceGroteskBold text-5xl leading-[4.2rem] '>
+              Our vision is to update you <br /> with international standard Python knowledge
             </h2>
           </div>
         </motion.div>
-        <div className='grid gap-4 md:gap-8 grid-cols-3 items-center p-3 mt-10'>
-          {stackCard.map((stack) => (
-            <div
-              key={stack.id}
-              className='col-span-3 md:col-span-1 bg-white shadow-2xl shadow-blue-500/10 rounded-lg h-full'
-            >
-              <div className='p-8'>
-                <BlurrableImage
-                  blurDataURl={blurImgUrl}
-                  className='h-40 w-60 mx-auto relative'
-                  img={
-                    <img
-                      src={stack.cardImg}
-                      loading='lazy'
-                      height='400'
-                      width='400'
-                      className='mx-auto h-40 w-auto'
-                      alt='card-img'
-                    />
-                  }
+        <div>
+          <div className=' text-center mb-6'>
+            <span className='py-2 from-[#D99BFF] from-[18%] to-[#D99BFF]/70 bg-gradient-to-l text-dark text-xl transition-all duration-400 hover:scale-105 cursor-pointer px-4'>
+              Developer Guidance
+            </span>
+          </div>
+          <div className='grid grid-cols-2 gap-x-10 gap-y-5  pb-10 pt-10  overflow-hidden relative'>
+            <div className='border-[#404041a4] border rounded-lg   md:pt-6 md:pb-8  card-side-border'>
+              <div className='py-8 px-5 rounded-lg flex space-x-5 items-center h-full'>
+                <img
+                  alt=''
+                  loading='lazy'
+                  width='72'
+                  height='46'
+                  decoding='async'
+                  data-nimg='1'
+                  className='w-[72px]'
+                  src='images/guidance-icon-1.svg'
                 />
-              </div>
-              <div className='px-4'>
-                <h2 className='text-center font-bold mt-4 text-sky-500 '>{stack.stackName}</h2>
-                <p className='text-slate-500 tracking-normal mt-3 mb-5 text-justify '>
-                  {stack.body}
+                <p className='text-text'>
+                  Connect with peers, industry experts, and potential employers to expand their
+                  network.
                 </p>
               </div>
             </div>
-          ))}
+            <div className='border-[#404041a4] border md:pt-6 md:pb-8  rounded-lg'>
+              <div className='py-8 px-5 rounded-lg flex space-x-5 items-center h-full'>
+                <img
+                  alt=''
+                  loading='lazy'
+                  width='72'
+                  height='46'
+                  decoding='async'
+                  data-nimg='1'
+                  className='w-[72px]'
+                  src='images/guidance-icon-2.svg'
+                />
+                <p className='text-text'>
+                  Expert-led sessions to guide them through the maze of development challenges.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className='my-3 hidden md:block ml-0 md:-ml-6'>
+            <div className='flex items-center justify-center'>
+              <span className='px-5 py-2 from-[#85B6FF] from-[18%] to-[#85B6FF]/70 bg-gradient-to-l text-dark text-xl transition-all duration-400 hover:scale-105 cursor-pointer'>
+                Networking
+              </span>
+              <div className="px-16 lg:px-28 mx-3 after:content-[''] after:left-0 after:top-1/2 after:translate-y-1/2 after:-z-[1] after:bg-white after:h-1 after:w-full relative after:absolute">
+                <h2 className='text-3xl font-SpaceGroteskLight text-white visionglance px-5 py-1'>
+                  {'>'}_academy.
+                  <span className='font-SpaceGroteskBold bg-clip-text text-transparent bg-gradient-to-br from-sky-400 from-30%  to-blue-600 to-55%'>
+                    py
+                  </span>{' '}
+                </h2>
+              </div>
+              <span className='px-5 py-2 from-[#FF9A62] from-[18%] to-[#FF9A62]/70 bg-gradient-to-l text-dark text-xl transition-all duration-400 hover:scale-105 cursor-pointer'>
+                Learning
+              </span>
+            </div>
+          </div>
+          <div className='grid grid-cols-2 gap-x-10 gap-y-5  overflow-hidden relative pb-10 pt-10 '>
+            <div className='border-[#404041a4] border rounded-lg  md:pt-8 md:pb-6  card-side-border'>
+              <div className='  py-8 px-5  flex space-x-5 items-center h-full'>
+                <img
+                  alt=''
+                  loading='lazy'
+                  width='72'
+                  height='46'
+                  decoding='async'
+                  data-nimg='1'
+                  className='w-[72px]'
+                  src='images/guidance-icon-3.svg'
+                />
+                <p className='text-text'>
+                  Join a supportive community of developers and collaborate on exciting projects.
+                </p>
+              </div>
+            </div>
+            <div className='border-[#404041a4] border rounded-lg  md:pt-8 md:pb-6 py-3'>
+              <div className=''>
+                <div className=' py-8 px-5 rounded-lg flex space-x-5 items-center h-full'>
+                  <img
+                    alt=''
+                    loading='lazy'
+                    width='72'
+                    height='46'
+                    decoding='async'
+                    data-nimg='1'
+                    className='w-[72px]'
+                    src='images/guidance-icon-4.svg'
+                  />
+                  <p className='text-text'>
+                    Acquire new skills and stay up-to-date with the latest trends in tech.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className=' text-center mt-6'>
+            <span className='px-6 py-2 from-[#FFD233] from-[18%] to-[#FFD233]/70 bg-gradient-to-l   transition-all duration-400 hover:scale-105 cursor-pointer'>
+              Community
+            </span>
+          </div>
         </div>
       </div>
     </div>
--- app/components/jurney/jurney.tsx
+++ app/components/jurney/jurney.tsx
@@ -1,90 +1,94 @@
 import { motion } from 'framer-motion'
 import { Info } from 'lucide-react'
+import Vision from '../Vision/vision'
 
 const Jurney = () => {
   return (
-    <div
-      className='relative bg-cover '
-      style={{
-        backgroundImage: "url('/images/star-bg.svg')",
-      }}
-    >
-      <div className='container'>
-        <motion.div
-          className='mx-auto w-11/12 mt-24 md:mb-16 md:mt-32 '
-          initial={{ opacity: 0, y: 25 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          viewport={{ once: true }}
-          transition={{ duration: 0.5 }}
-        >
-          <div>
-            <h2 className='font-SpaceGroteskBold text-5xl leading-[4.2rem]'>
-              {`>`}_ Academy.py at a glance <br />
-              Let's have a look. How exciting its <br /> journey going to be
-            </h2>
-          </div>
-        </motion.div>
+    <>
+      <div
+        className='relative bg-cover'
+        style={{
+          backgroundImage: "url('/images/star-bg.svg')",
+        }}
+      >
+        <div className='container'>
+          <motion.div
+            className='mx-auto w-11/12 mt-24 md:mb-16 md:mt-32 '
+            initial={{ opacity: 0, y: 25 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.5 }}
+          >
+            <div>
+              <h2 className='font-SpaceGroteskBold text-5xl leading-[4.2rem]'>
+                {`>`}_ Academy.py at a glance <br />
+                Let's have a look. How exciting its <br /> journey going to be
+              </h2>
+            </div>
+          </motion.div>
 
-        <div className=''>
-          <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-8 lg:gap-12'>
-            <div className='visionglance visionGlance-1 cursor-pointer  hover:bg-slate-900 flex items-center px-5 py-6 border border-slate-600/40 rounded-full group transition-all  hover:border-lime-800'>
-              <button className='w-12 h-12  rounded-full grid place-items-center bg-slate-800 group-hover:ring-1 transition-all group-hover:shadow-[0px_0px_15px] group-hover:ring-lime-500 group-hover:shadow-lime-500/40 mr-2'>
-                <Info strokeWidth={0.75} />
-              </button>
-              <p>Installation editor tools & environment setup </p>
-            </div>
-            <div className='visionglance  visionGlance-1 cursor-pointer  hover:bg-slate-900 flex items-center px-5 py-6 border border-slate-600/40 rounded-full group transition-all  hover:border-lime-800'>
-              <button className='w-12 h-12  rounded-full grid place-items-center bg-slate-800 group-hover:ring-1 transition-all group-hover:shadow-[0px_0px_15px] group-hover:ring-lime-500 group-hover:shadow-lime-500/40 mr-3'>
-                <Info strokeWidth={0.75} />
-              </button>
-              <p>Installation editor tools & environment setup </p>
-            </div>
-            <div className='visionglance   cursor-pointer  hover:bg-slate-900 flex items-center px-5 py-6 border border-slate-600/40 rounded-full group transition-all  hover:border-lime-800'>
-              <button className='w-12 h-12  rounded-full grid place-items-center bg-slate-800 group-hover:ring-1 transition-all group-hover:shadow-[0px_0px_15px] group-hover:ring-lime-500 group-hover:shadow-lime-500/40 mr-3'>
-                <Info strokeWidth={0.75} />
-              </button>
-              <p>Installation editor tools & environment setup </p>
-            </div>
-            <div className='visionglance  visionGlance-1  cursor-pointer  hover:bg-slate-900 flex items-center px-5 py-6 border border-slate-600/40 rounded-full group transition-all  hover:border-lime-800'>
-              <button className='w-12 h-12  rounded-full grid place-items-center bg-slate-800 group-hover:ring-1 transition-all group-hover:shadow-[0px_0px_15px] group-hover:ring-lime-500 group-hover:shadow-lime-500/40 mr-3'>
-                <Info strokeWidth={0.75} />
-              </button>
-              <p>Installation editor tools & environment setup </p>
-            </div>
-            <div className='visionglance  visionGlance-1 cursor-pointer  hover:bg-slate-900 flex items-center px-5 py-6 border border-slate-600/40 rounded-full group transition-all  hover:border-lime-800'>
-              <button className='w-12 h-12  rounded-full grid place-items-center bg-slate-800 group-hover:ring-1 transition-all group-hover:shadow-[0px_0px_15px] group-hover:ring-lime-500 group-hover:shadow-lime-500/40 mr-3'>
-                <Info strokeWidth={0.75} />
-              </button>
-              <p>Installation editor tools & environment setup </p>
-            </div>
-            <div className=' visionglance   visionGlance-2 cursor-pointer  hover:bg-slate-900 flex items-center px-5 py-6 border border-slate-600/40 rounded-full group transition-all  hover:border-lime-800'>
-              <button className='w-12 h-12  rounded-full grid place-items-center bg-slate-800 group-hover:ring-1 transition-all group-hover:shadow-[0px_0px_15px] group-hover:ring-lime-500 group-hover:shadow-lime-500/40 mr-3'>
-                <Info strokeWidth={0.75} />
-              </button>
-              <p>Installation editor tools & environment setup </p>
-            </div>
-            <div className='visionglance visionGlance-1 visionGlance-3   cursor-pointer  hover:bg-slate-900 flex items-center px-5 py-6 border border-slate-600/40 rounded-full group transition-all  hover:border-lime-800'>
-              <button className='border border-spacing-1 hover:bg-sky-500 border-slate-800/40 p-2 rounded-full mr-2'>
-                <Info strokeWidth={0.75} />
-              </button>
-              <p>Installation editor tools & environment setup </p>
-            </div>
-            <div className='visionglance visionGlance-1  cursor-pointer  hover:bg-slate-900 flex items-center px-5 py-6 border border-slate-600/40 rounded-full group transition-all  hover:border-lime-800'>
-              <button className='w-12 h-12  rounded-full grid place-items-center bg-slate-800 group-hover:ring-1 transition-all group-hover:shadow-[0px_0px_15px] group-hover:ring-lime-500 group-hover:shadow-lime-500/40 mr-3'>
-                <Info strokeWidth={0.75} />
-              </button>
-              <p>Installation editor tools & environment setup </p>
-            </div>
-            <div className='visionglance  cursor-pointer  hover:bg-slate-900 flex items-center px-5 py-6 border border-slate-600/40 rounded-full group transition-all  hover:border-lime-800'>
-              <button className='w-12 h-12  rounded-full grid place-items-center bg-slate-800 group-hover:ring-1 transition-all group-hover:shadow-[0px_0px_15px] group-hover:ring-lime-500 group-hover:shadow-lime-500/40 mr-3'>
-                <Info strokeWidth={0.75} />
-              </button>
-              <p>Installation editor tools & environment setup </p>
+          <div className=''>
+            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-8 lg:gap-12'>
+              <div className='visionglance visionGlance-1 cursor-pointer  hover:bg-slate-900 flex items-center px-5 py-6 border border-slate-600/40 rounded-full group transition-all  hover:border-lime-800'>
+                <button className='w-12 h-12  rounded-full grid place-items-center bg-slate-800 group-hover:ring-1 transition-all group-hover:shadow-[0px_0px_15px] group-hover:ring-lime-500 group-hover:shadow-lime-500/40 mr-2'>
+                  <Info strokeWidth={0.75} />
+                </button>
+                <p>Installation editor tools & environment setup </p>
+              </div>
+              <div className='visionglance  visionGlance-1 cursor-pointer  hover:bg-slate-900 flex items-center px-5 py-6 border border-slate-600/40 rounded-full group transition-all  hover:border-lime-800'>
+                <button className='w-12 h-12  rounded-full grid place-items-center bg-slate-800 group-hover:ring-1 transition-all group-hover:shadow-[0px_0px_15px] group-hover:ring-lime-500 group-hover:shadow-lime-500/40 mr-3'>
+                  <Info strokeWidth={0.75} />
+                </button>
+                <p>Installation editor tools & environment setup </p>
+              </div>
+              <div className='visionglance   cursor-pointer  hover:bg-slate-900 flex items-center px-5 py-6 border border-slate-600/40 rounded-full group transition-all  hover:border-lime-800'>
+                <button className='w-12 h-12  rounded-full grid place-items-center bg-slate-800 group-hover:ring-1 transition-all group-hover:shadow-[0px_0px_15px] group-hover:ring-lime-500 group-hover:shadow-lime-500/40 mr-3'>
+                  <Info strokeWidth={0.75} />
+                </button>
+                <p>Installation editor tools & environment setup </p>
+              </div>
+              <div className='visionglance  visionGlance-1  cursor-pointer  hover:bg-slate-900 flex items-center px-5 py-6 border border-slate-600/40 rounded-full group transition-all  hover:border-lime-800'>
+                <button className='w-12 h-12  rounded-full grid place-items-center bg-slate-800 group-hover:ring-1 transition-all group-hover:shadow-[0px_0px_15px] group-hover:ring-lime-500 group-hover:shadow-lime-500/40 mr-3'>
+                  <Info strokeWidth={0.75} />
+                </button>
+                <p>Installation editor tools & environment setup </p>
+              </div>
+              <div className='visionglance  visionGlance-1 cursor-pointer  hover:bg-slate-900 flex items-center px-5 py-6 border border-slate-600/40 rounded-full group transition-all  hover:border-lime-800'>
+                <button className='w-12 h-12  rounded-full grid place-items-center bg-slate-800 group-hover:ring-1 transition-all group-hover:shadow-[0px_0px_15px] group-hover:ring-lime-500 group-hover:shadow-lime-500/40 mr-3'>
+                  <Info strokeWidth={0.75} />
+                </button>
+                <p>Installation editor tools & environment setup </p>
+              </div>
+              <div className=' visionglance   visionGlance-2 cursor-pointer  hover:bg-slate-900 flex items-center px-5 py-6 border border-slate-600/40 rounded-full group transition-all  hover:border-lime-800'>
+                <button className='w-12 h-12  rounded-full grid place-items-center bg-slate-800 group-hover:ring-1 transition-all group-hover:shadow-[0px_0px_15px] group-hover:ring-lime-500 group-hover:shadow-lime-500/40 mr-3'>
+                  <Info strokeWidth={0.75} />
+                </button>
+                <p>Installation editor tools & environment setup </p>
+              </div>
+              <div className='visionglance visionGlance-1 visionGlance-3   cursor-pointer  hover:bg-slate-900 flex items-center px-5 py-6 border border-slate-600/40 rounded-full group transition-all  hover:border-lime-800'>
+                <button className='w-12 h-12  rounded-full grid place-items-center bg-slate-800 group-hover:ring-1 transition-all group-hover:shadow-[0px_0px_15px] group-hover:ring-lime-500 group-hover:shadow-lime-500/40 mr-2'>
+                  <Info strokeWidth={0.75} />
+                </button>
+                <p>Installation editor tools & environment setup </p>
+              </div>
+              <div className='visionglance visionGlance-1  cursor-pointer  hover:bg-slate-900 flex items-center px-5 py-6 border border-slate-600/40 rounded-full group transition-all  hover:border-lime-800'>
+                <button className='w-12 h-12  rounded-full grid place-items-center bg-slate-800 group-hover:ring-1 transition-all group-hover:shadow-[0px_0px_15px] group-hover:ring-lime-500 group-hover:shadow-lime-500/40 mr-3'>
+                  <Info strokeWidth={0.75} />
+                </button>
+                <p>Installation editor tools & environment setup </p>
+              </div>
+              <div className='visionglance  cursor-pointer  hover:bg-slate-900 flex items-center px-5 py-6 border border-slate-600/40 rounded-full group transition-all  hover:border-lime-800'>
+                <button className='w-12 h-12  rounded-full grid place-items-center bg-slate-800 group-hover:ring-1 transition-all group-hover:shadow-[0px_0px_15px] group-hover:ring-lime-500 group-hover:shadow-lime-500/40 mr-3'>
+                  <Info strokeWidth={0.75} />
+                </button>
+                <p>Installation editor tools & environment setup </p>
+              </div>
             </div>
           </div>
         </div>
+        <Vision />
       </div>
-    </div>
+    </>
   )
 }
 
